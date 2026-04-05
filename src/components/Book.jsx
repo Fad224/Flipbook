9f7a6a6fbfa58c96b2244f4abcf851068d568475
Fadi Azzam <fad224@gmail.com>
2026-04-05 08:34:06 +0400

    First Commit

---
+++ src/components/Book.jsx
@@ -0,0 +1,89 @@
+import React, { useRef, useEffect, useState } from 'react'
+import HTMLFlipBook from "react-pageflip";
+import frontCover from '../../assets/FrontCover.png';
+import page2 from '../../assets/Fadi Azzam Portfolio - 2.png';
+import page3 from '../../assets/Fadi Azzam Portfolio - 3.png';
+import page4 from '../../assets/Fadi Azzam Portfolio - 4.png';
+import page5 from '../../assets/Fadi Azzam Portfolio - 5.png';
+import page6 from '../../assets/Fadi Azzam Portfolio - 6.png';
+import page7 from '../../assets/Fadi Azzam Portfolio - 7.png';
+import page8 from '../../assets/Fadi Azzam Portfolio - 8.png';
+import page9 from '../../assets/Fadi Azzam Portfolio - 9.png';
+import page10 from '../../assets/Fadi Azzam Portfolio - 10.png';
+import page11 from '../../assets/Fadi Azzam Portfolio - 11.png';
+import page12 from '../../assets/Fadi Azzam Portfolio - 12.png';
+import page13 from '../../assets/Fadi Azzam Portfolio - 13.png';
+import backCover from '../../assets/BackCover.png';
+
+function Book() {
+
+  const bookRef = useRef();
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (isAnimating) return;
+      if (event.key === 'ArrowLeft') {
+        setIsAnimating(true);
+        bookRef.current.pageFlip().flipPrev();
+      } else if (event.key === 'ArrowRight') {
+        setIsAnimating(true);
+        bookRef.current.pageFlip().flipNext();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [isAnimating]);
+
+  const pages = [
+    frontCover,
+    page2,
+    page3,
+    page4,
+    page5,
+    page6,
+    page7,
+    page8,
+    page9,
+    page10,
+    page11,
+    page12,
+    page13,
+    backCover
+  ];
+
+  return (
+    <HTMLFlipBook 
+      ref={bookRef}
+      width={707} 
+      height={500}
+      maxShadowOpacity={0.5}
+      drawShadow={true}
+      showCover={true}
+      size='fixed'
+      onChangeState={(e) => {
+        if (e.data !== 'flipping') {
+          setIsAnimating(false);
+        }
+      }}
+    >
+      {pages.map((src, index) => (
+        <div className="page" key={index}>
+          <div className="page-content">
+            <img 
+              src={src} 
+              alt={`Page ${index + 1}`} 
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            />
+          </div>
+        </div>
+      ))}
+    </HTMLFlipBook>
+  );
+}
+
+export default Book
